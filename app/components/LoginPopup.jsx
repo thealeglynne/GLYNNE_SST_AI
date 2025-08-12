1ff871017781344dba90d30017cb15b863ec0cb2
FlowTraker <flowtrakersealab@gmail.com>
2025-08-12 10:26:17 -0500

    ee

--- app/components/LoginPopup.jsx
+++ app/components/LoginPopup.jsx
@@ -15,7 +15,7 @@ export function LoginPopup({ visible, onClose }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/menu`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
       },
     });
 
@@ -42,7 +42,7 @@ export function LoginPopup({ visible, onClose }) {
           await supabase.from('GLNNEacces').insert([{ email: user.email }]);
         }
 
-        router.push('/menu');
+        router.push('/');
       }
     };
 
